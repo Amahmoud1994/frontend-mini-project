8b61d2d974413c98914baaf79daa30123bc4e3f4
Abkreno <mohamedelhamamsy789@gmail.com>
2017-03-28 23:59:38 +0200

    Adds search bar border

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ var handler = function(){
   $(this).children('.dropdown__list').stop().slideToggle(200);
 }
 $( '.dropdown' ).hover( handler );
+
+$(window).click(function() {
+  $('.search-form__icon').toggleClass('search-form__icon__active');
+});
+
+$('.search-form__icon').click(function(event){
+    event.stopPropagation();
+    console.log("here");
+    $('.search-form__icon').toggleClass('search-form__icon__active');
+});
